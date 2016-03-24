bec440594b179a2ce48b2d4bec9bdf94a632d671
Carl Ritson <cgr@kent.ac.uk>
2016-03-24 15:58:41 +0000

    Merge install-conf into unpack.

--- unpack.sh
+++ unpack.sh
@@ -25,3 +25,13 @@ source env.sh
 pushd bench
 sbt package
 popd
+
+#ln -sf ../../../conf/core-site.xml hadoop-2.6.0/etc/hadoop/core-site.xml
+#ln -sf ../../../conf/hdfs-site.xml hadoop-2.6.0/etc/hadoop/hdfs-site.xml
+#ln -sf ../../../conf/mapred-site.xml hadoop-2.6.0/etc/hadoop/mapred-site.xml
+#ln -sf ../../../conf/hadoop-env.sh hadoop-2.6.0/etc/hadoop/hadoop-env.sh
+
+ln -sf ../../conf/spark-defaults.conf spark-1.4.1-bin-hadoop2.6/conf/spark-defaults.conf 
+ln -sf ../../conf/spark-env.sh spark-1.4.1-bin-hadoop2.6/conf/spark-env.sh
+ln -sf ../../conf/log4j.properties spark-1.4.1-bin-hadoop2.6/conf/log4j.properties
+
